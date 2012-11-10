d805dcecd9b60b951eb52364218e4d873bf3af70
Charaf Salmi <efrei@charafsalmi.org>
2012-11-10 20:33:18 +0100

    packages created

---
+++ src/node/Node.java
@@ -0,0 +1,14 @@
+package node;
+
+public class Node {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a node");
+
+	}
+
+}
---
+++ src/tests/Tests.java
@@ -0,0 +1,13 @@
+package tests;
+
+public class Tests {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Application for launching the tests");
+	}
+
+}
