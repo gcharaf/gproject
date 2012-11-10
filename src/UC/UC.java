fa572129ac56d1ec665e4a4f929b92aa46b27bb6
Charaf Salmi <efrei@charafsalmi.org>
2012-11-10 20:23:44 +0100

    Project creation

---
+++ src/UC/UC.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package UC;
+
+/**
+ * @author Charaf
+ *
+ */
+public class UC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+		System.out.println("This is the UC application");
+
+	}
+
+}
